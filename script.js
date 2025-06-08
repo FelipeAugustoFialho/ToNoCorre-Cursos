90421078853231bad4dd2a8cb5bfadaa8c64471d
FelipeAugustoFialho <felipefialho.informatica@gmail.com>
2025-06-08 12:47:29 -0300

    add nav, footer

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 const all = document.querySelector(".showAll");
 
+
+const menu = document.querySelector("navMobile");
+const list = document.querySelector('li');
+const menuIcon = document.querySelector('.menu-icon');
+const menuOptions = document.querySelector('.menu-options');
+
+
+function toggleMenu() {
+    menuOptions.classList.toggle('active');
+}
+
+
 function msg(){
     alert("No momento não há mais páginas de cursos disponíveis")
 }
 
+menuIcon.addEventListener('click', toggleMenu);
 all.addEventListener('click',msg);
